252ce75521792a8cf37a355963970958b4e031f5
John Rey Lumangyao <lumangyao112@gmail.com>
2024-03-04 17:10:05 +0800

    [feature][chappy] Create admin dashboard

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+
+export default function AuthLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className=" flex flex-row">
+      <div className=" w-1/5 h-screen text-white bg-red-600 p-3">
+        <div className=" w-full ">
+          <nav>
+            <ul className=" w-full">
+              <Link href={"/dashboard"}>
+                <li className=" hover:bg-red-800 w-full cursor-pointer p-3 text-sm">
+                  Dashboard
+                </li>
+              </Link>
+              <Link href={"/user"}>
+                <li className=" hover:bg-red-800 w-full cursor-pointer p-3 text-sm">
+                  Users
+                </li>
+              </Link>
+              <li className=" hover:bg-red-800 w-full cursor-pointer p-3 text-sm">
+                Admins
+              </li>
+              <li className=" hover:bg-red-800 w-full cursor-pointer p-3 text-sm">
+                Responder
+              </li>
+              <li className=" hover:bg-red-800 w-full cursor-pointer p-3 text-sm">
+                Logout
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+      <div className=" w-full">
+        <div className=" h-16 flex items-center  bg-red-500">
+          <p className="  p-2 font-bold text-lg text-white">
+            Quick Response Admin
+          </p>
+        </div>
+        {children}
+      </div>
+    </div>
+  );
+}
